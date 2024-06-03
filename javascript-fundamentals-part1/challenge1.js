45e5dcff2a6d2871617aef2f3862fc7ea91651cf
Muhammad Ali Khalid <148471160+muhammad-ali-khalid@users.noreply.github.com>
2024-06-03 22:16:39 +0500

    Create challenge1.js

---
+++ javascript-fundamentals-part1/challenge1.js
@@ -0,0 +1,8 @@
+const markMass = 78;
+const johnMass = 92;
+const markHeight = 1.69;
+const johnHeight = 1.95;
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+const markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
