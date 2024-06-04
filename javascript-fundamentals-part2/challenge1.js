9d5dc236d8cad1a139ad5214a7b2eb871193a3ea
Muhammad Ali Khalid <148471160+muhammad-ali-khalid@users.noreply.github.com>
2024-06-04 14:01:15 +0500

    Update challenge1.js

--- javascript-fundamentals-part2/challenge1.js
+++ javascript-fundamentals-part2/challenge1.js
@@ -1,1 +1,16 @@
-
+"use script";
+const calcAverage = (score1, score2, score3) =>
+  (score1 + score2 + score3) / 3;
+const checkWinner = (dolphinsAvg, koalasAvg) => {
+  if (dolphinsAvg >= 2 * koalasAvg)
+    console.log(`Dolphins wins with an average of ${dolphinsAvg}`);
+  else if (koalasAvg >= 2 * dolphinsAvg) {
+    console.log(`Koalas wins with an average of ${koalasAvg}`);
+  } else {
+    console.log("No team wins");
+  }
+};
+const dolphinsAvg = calcAverage(85, 54, 41);
+console.log(dolphinsAvg);
+const koalasAvg = calcAverage(23, 34, 27);
+checkWinner(dolphinsAvg, koalasAvg);
